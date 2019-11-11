3c6c23dd95dea06e9f76511e186b6801b62533b6
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2019-11-11 13:12:10 +0100

    Input/output accoording to run.sh

--- main.cpp
+++ main.cpp
@@ -4,198 +4,124 @@
 #include "PDA.h"
 #include "CFG.h"
 
-void evaluate(PDA &pda, const std::string &evaluateString1);
-
-PDA voorbeeldPDA1() {
-    State *q0 = new State("q0");
-    State *q1 = new State("q1");
-    State *qf = new State("qf");
-
-    std::vector<State *> statesQ = {q0, q1, qf};
-
-    Transition *q0q0a = new Transition(q0, q0, {"a", "a", "a"}, 'a', "a");
-    Transition *q0q0Z = new Transition(q0, q0, {"a", "a", "Z"}, 'a', "Z");
-    Transition *q0q1b = new Transition(q0, q1, {"e"}, 'b', "a");
-    Transition *q1q1b = new Transition(q1, q1, {"e"}, 'b', "a");
-    Transition *q1qf = new Transition(q1, qf, {"Z"}, 'e', "Z");
-
-    std::vector<Transition *> transitionFunctionD = {q0q0a, q0q0Z, q0q1b, q1q1b, q1qf};
-
-    std::string startStack = "Z";
-
-    std::vector<char> inputAlphabetS = {'a', 'b'};
-
-    std::vector<std::string> stackAlphabetG = {"a", "b", "Z"};
-
-    State *startState = q0;
-
-    std::vector<State *> endStatesF = {qf};
-
-
-    PDA pda(statesQ, inputAlphabetS, stackAlphabetG, startState, startStack, endStatesF, transitionFunctionD);
-
-    return pda;
-}
-
-//PDA testPDA1() {
-//    State *q0 = new State("q0");
-//
-//    std::vector<State *> statesQ = {q0};
-//
-//    Transition *q0q0a = new Transition(q0, q0, "a", 'a', 'a');
-//
-//    std::vector<Transition *> transitionFunctionD = {q0q0a};
-//
-//    char startStack = 'a';
-//
-//    std::vector<char> inputAlphabetS = {'a'};
-//
-//    std::vector<char> stackAlphabetG = {'a'};
-//
-//    State *startState = q0;
-//
-//    std::vector<State *> endStatesF = {q0};
-//
-//
-//    PDA pda(statesQ, inputAlphabetS, stackAlphabetG, startState, startStack, endStatesF, transitionFunctionD);
-//
-//    return pda;
-//}
-
-//PDA testPDA2() {
-//    State *q0 = new State("q0");
-//    State *q1 = new State("q1");
-//
-//    std::vector<State *> statesQ = {q0, q1};
-//
-//    Transition *q0q1a = new Transition(q0, q1, "b", 'a', 'a');
-//
-//    std::vector<Transition *> transitionFunctionD = {q0q1a};
-//
-//    char startStack = 'a';
-//
-//    std::vector<char> inputAlphabetS = {'a'};
-//
-//    std::vector<char> stackAlphabetG = {'a', 'b'};
-//
-//    State *startState = q0;
-//
-//    std::vector<State *> endStatesF = {q1};
-//
-//
-//    PDA pda(statesQ, inputAlphabetS, stackAlphabetG, startState, startStack, endStatesF, transitionFunctionD);
-//
-//    return pda;
-//}
-
-//PDA testPDA3() {
-//    State *q0 = new State("q0");
-//    State *q1 = new State("q1");
-//    State *q2 = new State("q2");
-//
-//    std::vector<State *> statesQ = {q0, q1, q2};
-//
-//    Transition *q0q1a = new Transition(q0, q1, "b", 'a', 'a');
-//    Transition *q0q2a = new Transition(q0, q2, "a", 'a', 'b');
-//
-//    std::vector<Transition *> transitionFunctionD = {q0q1a, q0q2a};
-//
-//    char startStack = 'b'; // a is accepting, b rejecting bij string "a"
-//
-//    std::vector<char> inputAlphabetS = {'a'};
-//
-//    std::vector<char> stackAlphabetG = {'a', 'b'};
-//
-//    State *startState = q0;
-//
-//    std::vector<State *> endStatesF = {q1};
-//
-//
-//    PDA pda(statesQ, inputAlphabetS, stackAlphabetG, startState, startStack, endStatesF, transitionFunctionD);
-//
-//    return pda;
-//}
-
-
-
-int main() {
+int main(int argc, char *argv[]) {
 
     std::cout << "Hello, World!" << std::endl;
 
-//    PDA pda = voorbeeldPDA1();
-//    PDA pda = testPDA3();
-
-    std::string filename = "pdaReeks5Oef4.json";
-    std::vector<State *> states = {};
-    std::vector<char> alphabet = {};
-    std::vector<std::string> stackAlphabet = {};
-    std::vector<Transition *> transition_function = {};
-    State *start_state;
-    std::string start_stack;
-    std::vector<State *> final_states;
-
-    read_json(filename,
-                   states,
-                   alphabet,
-                   stackAlphabet,
-                   transition_function,
-                   start_state,
-                   start_stack,
-                   final_states);
-
-    PDA pda = PDA(states, alphabet, stackAlphabet, start_state, start_stack, final_states, transition_function);
-
-    std::string evaluateString1 = "aabbbb";
-    std::string evaluateString2 = "aab";
-
-    evaluate(pda, evaluateString1);
-    evaluate(pda, evaluateString2);
-
-    pda.toDot("pda");
-
-    CFG* cfg = pda.toCFG(); // TODO: check on too many productions
-    cfg->toJSON("pdaToCFG.json");
-
-    std::vector<std::string> nonTerminalsV;
-    std::vector<std::string> terminalsT;
-    std::vector<Production *> productionsP;
-    std::string startS;
-    readJson("Reeks7Oef2.json", nonTerminalsV, terminalsT, productionsP, startS);
-
-    cfg = new CFG(nonTerminalsV, terminalsT, productionsP, startS);
-
-    cfg->toCNF();
-    cfg->toJSON("cfgToCNF.json");
-
-    pda.toCFG();
-
-    readJson("Reeks7Oef3.json", nonTerminalsV, terminalsT, productionsP, startS);
-    cfg = new CFG(nonTerminalsV, terminalsT, productionsP, startS);
-
-
-    std::ofstream file;
-    file.open ("output.html");
-
-    std::string html;
-    if(cfg->toCYK("abbc", html)){
-        std::cout << "Succes." << std::endl;
-    } else {
-        std::cout << "Failed." << std::endl;
+    if (argc < 4) { // ./indiv type input output is always required
+        std::cerr << "ERROR: Not enough arguments: At least 3 required: ./indiv [type] [input] <string> [output]"
+                  << std::endl;
+        return 1;
     }
 
-    file << html;
-    file.close();
+    std::string type = argv[1];
+
+    if (type == "pda2cfg") {
+
+        std::string inputFile = argv[2];
+
+        std::vector<State *> states = {};
+        std::vector<char> alphabet = {};
+        std::vector<std::string> stackAlphabet = {};
+        std::vector<Transition *> transition_function = {};
+        State *start_state;
+        std::string start_stack;
+        std::vector<State *> final_states;
+
+        read_json(inputFile,
+                  states,
+                  alphabet,
+                  stackAlphabet,
+                  transition_function,
+                  start_state,
+                  start_stack,
+                  final_states);
+
+        PDA pda = PDA(states, alphabet, stackAlphabet, start_state, start_stack, final_states, transition_function);
+
+        CFG *cfg = pda.toCFG();
+
+        std::string outputFile = argv[3];
+
+        // Source: Nawaz (2012). How to redirect cin and cout to files. Visited on November 11, 2019 via https://stackoverflow.com/questions/10150468/how-to-redirect-cin-and-cout-to-files.
+        std::ofstream out(outputFile);
+        std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
+        std::cout.rdbuf(out.rdbuf()); //redirect std::cout to outputFile!
+
+        cfg->printReadable();
+
+        std::cout.rdbuf(coutbuf); //reset to standard output again
+
+    } else if (type == "cnf") {
+
+        std::string inputFile = argv[2];
+
+        std::vector<std::string> nonTerminalsV;
+        std::vector<std::string> terminalsT;
+        std::vector<Production *> productionsP;
+        std::string startS;
+
+        readJson(inputFile, nonTerminalsV, terminalsT, productionsP, startS);
+
+        CFG* cfg = new CFG(nonTerminalsV, terminalsT, productionsP, startS);
+
+        cfg->toCNF();
+
+        std::string outputFile = argv[3];
+
+        // Source: Nawaz (2012). How to redirect cin and cout to files. Visited on November 11, 2019 via https://stackoverflow.com/questions/10150468/how-to-redirect-cin-and-cout-to-files.
+        std::ofstream out(outputFile);
+        std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
+        std::cout.rdbuf(out.rdbuf()); //redirect std::cout to outputFile!
+
+        cfg->printReadable();
+
+        std::cout.rdbuf(coutbuf); //reset to standard output again
+
+    } else if (type == "cyk") {
+
+        if (argc < 5) {
+            std::cerr << "Not enough arguments provided for cyk: ./indiv cyk [input] [string] [output]" << std::endl;
+            return 1;
+        }
+
+        std::string inputFile = argv[2];
+
+        std::vector<std::string> nonTerminalsV;
+        std::vector<std::string> terminalsT;
+        std::vector<Production *> productionsP;
+        std::string startS;
+
+        readJson(inputFile, nonTerminalsV, terminalsT, productionsP, startS);
+
+        CFG* cfg = new CFG(nonTerminalsV, terminalsT, productionsP, startS);
+
+        std::string evaluation = argv[3];
+        std::string outputFile = argv[4];
+
+        std::ofstream file;
+        file.open(outputFile);
+
+        std::string html;
+        if (cfg->toCYK(evaluation, html)) {
+            file << "The string \"" << evaluation << "\" is accepted." << std::endl;
+        } else {
+            file << "The string \"" << evaluation << "\" is rejected." << std::endl;
+        }
+
+        std::string outputHtmlFile = outputFile + ".html";
+
+        file << "You can find the CYK-table in the file \"" << outputHtmlFile << "\"." << std::endl;
+
+        file.close();
+
+        file.open(outputHtmlFile);
+        file << html;
+        file.close();
+
+    } else {
+        std::cerr << "Type \"" << type << "\" not supported." << std::endl;
+    }
 
     return 0;
-}
-
-void evaluate(PDA &pda, const std::string &evaluateString1) {
-    switch (pda.evaluate(evaluateString1)) {
-        case true:
-            std::cout << "String " << evaluateString1 << " accepted" << std::endl;
-            break;
-        case false:
-            std::cout << "String " << evaluateString1 << " rejected" << std::endl;
-            break;
-    }
 }
