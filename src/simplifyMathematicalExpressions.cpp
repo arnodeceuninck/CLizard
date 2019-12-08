0aeb81826941bf1baf60af47cd729d5b70b97ac4
trinm <tim.sanders@student.uantwerpen.be>
2019-12-08 19:19:41 +0100

    SME update
    Divide operators en To PRN toegevoegd

--- src/simplifyMathematicalExpressions.cpp
+++ src/simplifyMathematicalExpressions.cpp
@@ -3,8 +3,64 @@
 //
 
 #include "simplifyMathematicalExpressions.h"
+#include <stack>
+//#include <stdio.h>
+#include <algorithm>
 
 int simplifyMathematicalExpressions(const std::vector<std::string> &inputFiles) {
+    //std::vector<std::string> test = divideNumbersOperators("((15 / (7 − (1 + 1))) × 3) − (2 + (1 + 1))");
+    std::vector<std::string> test = toPRN("((15/(7-(1+1)))*3)-(2+(1+1))");
     return 0;
 }
 
+std::vector<std::string> toPRN(std::string input) {
+    std::vector<std::string> output;
+    std::vector<std::string> operators{"+", "-", "*", "/", "(", ")"};
+    std::stack<std::string> stack;
+
+    std::vector<std::string> dividedInput = divideNumbersOperators(input);
+
+    for (int i = 0; i < dividedInput.size(); ++i) {
+        if(std::find(operators.begin(), operators.end(), dividedInput[i]) != operators.end()){
+            if(dividedInput[i] == ")"){
+                while (stack.top() != "(") {
+                    output.push_back(stack.top());
+                    stack.pop();
+                }
+                stack.pop();
+            }else{
+                stack.push(dividedInput[i]);
+            }
+        } else {
+            output.push_back(dividedInput[i]);
+        }
+    }
+    while(!stack.empty()){
+        output.push_back(stack.top());
+        stack.pop();
+    }
+
+    return output;
+}
+
+std::vector<std::string> divideNumbersOperators(std::string input) {
+    std::vector<std::string> output;
+    std::vector<std::string> operators{"+", "-", "*", "/", "(", ")"};
+    for (int i = 0; i < input.size(); ++i) {
+        if(input[i] == ' '){
+            continue;
+        }
+        if(std::find(operators.begin(), operators.end(), std::string(1,input[i])) != operators.end()){
+            std::string temp(1, input[i]);
+            output.push_back(temp);
+        } else{
+            int length = 1;
+            while(std::find(operators.begin(), operators.end(), std::string(1,input[i+length])) == operators.end() and i+length < input.size()){
+                length++;
+            }
+            output.push_back(input.substr(i, length));
+            i += length-1;
+        }
+    }
+    return output;
+}
--- src/simplifyMathematicalExpressions.h
+++ src/simplifyMathematicalExpressions.h
@@ -10,4 +10,8 @@
 
 int simplifyMathematicalExpressions(const std::vector<std::string> &inputFiles);
 
+std::vector<std::string> toPRN(std::string input);
+
+std::vector<std::string> divideNumbersOperators(std::string input);
+
 #endif //CLIZARD_SIMPLIFYMATHEMATICALEXPRESSIONS_H
