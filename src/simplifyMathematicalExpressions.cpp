faf28d7de9672009e33d3fb440e1bfeb21aade6a
trinm8 <tim.sanders@student.uantwerpen.be>
2019-12-09 16:11:38 +0100

    SME Workshop version

--- src/simplifyMathematicalExpressions.cpp
+++ src/simplifyMathematicalExpressions.cpp
@@ -4,34 +4,58 @@
 
 #include "simplifyMathematicalExpressions.h"
 #include <stack>
+#include <iostream>
 //#include <stdio.h>
+#include <iterator>
 #include <algorithm>
 
 int simplifyMathematicalExpressions(const std::vector<std::string> &inputFiles) {
     //std::vector<std::string> test = divideNumbersOperators("((15 / (7 − (1 + 1))) × 3) − (2 + (1 + 1))");
-    std::vector<std::string> test = toPRN("((15/(7-(1+1)))*3)-(2+(1+1))");
+    //std::vector<std::string> test = toPRN("((15/(7-(1+1)))*3)-(2+(1+1))");
+    //std::vector<std::string> test2 = simplify(test);
+    std::cout << toInfix(simplify(toPRN("((15/(7-(var+1)))*3)-(2+(1+1))"))) << std::endl;
+    std::cout << toInfix(simplify(toPRN("9/3*2"))) << std::endl;
     return 0;
 }
 
 std::vector<std::string> toPRN(std::string input) {
     std::vector<std::string> output;
-    std::vector<std::string> operators{"+", "-", "*", "/", "(", ")"};
+    std::vector<std::string> operators{"+", "-", "*", "/"};
     std::stack<std::string> stack;
 
     std::vector<std::string> dividedInput = divideNumbersOperators(input);
 
     for (int i = 0; i < dividedInput.size(); ++i) {
         if(std::find(operators.begin(), operators.end(), dividedInput[i]) != operators.end()){
-            if(dividedInput[i] == ")"){
+            while (!stack.empty() and (checkPrecedence(dividedInput[i]) < checkPrecedence(stack.top()) ||
+                    (checkPrecedence(dividedInput[i]) == checkPrecedence(stack.top()))) and stack.top() != "(") {
+                output.push_back(stack.top());
+                stack.pop();
+            }
+            stack.push(dividedInput[i]);
+/*            if(dividedInput[i] == ")"){
                 while (stack.top() != "(") {
                     output.push_back(stack.top());
                     stack.pop();
                 }
                 stack.pop();
             }else{
-                stack.push(dividedInput[i]);
+                while(checkPrecedence(dividedInput[i]) < checkPrecedence(stack.top())){
+                    output.push_back(stack.top());
+                    stack.pop();
+                }
+            }*/
+        } else if (dividedInput[i] == "("){
+            stack.push("(");
+        }else if (dividedInput[i] == ")"){
+            while(stack.top() != "("){
+                output.push_back(stack.top());
+                stack.pop();
             }
-        } else {
+            if(stack.top() == "("){
+                stack.pop();
+            }
+        }else{
             output.push_back(dividedInput[i]);
         }
     }
@@ -63,4 +87,89 @@ std::vector<std::string> divideNumbersOperators(std::string input) {
         }
     }
     return output;
+}
+
+bool isNumber(std::string input) {
+    for (int i = 0; i < input.size(); ++i) {
+        if(!std::isdigit(input[i])){
+            if(input[i] == '.'){
+                continue;
+            }
+            return false;
+        }
+    }
+    return true;
+}
+
+std::string toInfix(std::vector<std::string> input) {
+    std::stack<std::string> stack;
+    std::vector<std::string> operators{"+", "-", "*", "/"};
+    std::string output;
+    for (int i = 0; i < input.size(); ++i) {
+        if(std::find(operators.begin(), operators.end(), input[i]) != operators.end()){
+            std::string x2 = stack.top();
+            stack.pop();
+            std::string x1 = stack.top();
+            stack.pop();
+            stack.push(std::string("(" + x1 + input[i] + x2 + ")"));
+        } else{
+            stack.push(input[i]);
+        }
+    }
+    output = stack.top();
+    if(*output.begin() == '(' and *output.end() == ')') {
+        output.pop_back();
+        output.erase(output.begin());
+    }
+    return output;
+}
+
+std::vector<std::string> simplify(std::vector<std::string> input) {
+    std::vector<std::string>::iterator pos = input.begin();
+    std::vector<std::string> operators{"+", "-", "*", "/"};
+    while(pos != input.end() && std::next(pos) != input.end()){
+        while(std::next(pos) != input.end() && !isOperator(*std::next(pos))){
+            pos++;
+        }
+
+        float temp;
+        if(isNumber(*pos) && isNumber(*std::prev(pos))){
+            std::string operatorToken = *std::next(pos);
+            if(operatorToken == "+"){
+                 temp = std::stof(*std::prev(pos)) + std::stof(*pos);
+            } else if (operatorToken == "-"){
+                temp = std::stof(*std::prev(pos)) - std::stof(*pos);
+            } else if (operatorToken == "/"){
+                temp = std::stof(*std::prev(pos)) / std::stof(*pos);
+            } else if (operatorToken == "*"){
+                temp = std::stof(*std::prev(pos)) * std::stof(*pos);
+            }
+            *pos = std::to_string(temp);
+            input.erase(std::next(pos));
+            input.erase(std::prev(pos));
+            pos = std::prev(pos);
+        } else{
+            pos++;
+        }
+    }
+
+    return input;
+}
+
+bool isOperator(std::string input){
+    if(input.size() > 1){
+        return false;
+    }
+    return (input <= "a" || input >= "z") && (input <= "A" || input >= "Z") && (input <= "0" || input >= "9");
+}
+
+int checkPrecedence(std::string op) {
+    std::vector<std::string> precedence1 = {"+", "-"};
+    std::vector<std::string> precedence2 = {"*", "/"};
+    if(std::find(precedence1.begin(), precedence1.end(), op) != precedence1.end()){
+        return 1;
+    } else if(std::find(precedence2.begin(), precedence2.end(), op) != precedence2.end()){
+        return 2;
+    }
+    return 0;
 }
--- src/simplifyMathematicalExpressions.h
+++ src/simplifyMathematicalExpressions.h
@@ -14,4 +14,14 @@ std::vector<std::string> toPRN(std::string input);
 
 std::vector<std::string> divideNumbersOperators(std::string input);
 
+bool isNumber(std::string input);
+
+std::string toInfix(std::vector<std::string> input);
+
+std::vector<std::string> simplify(std::vector<std::string> input);
+
+bool isOperator(std::string input);
+
+int checkPrecedence(std::string op);
+
 #endif //CLIZARD_SIMPLIFYMATHEMATICALEXPRESSIONS_H
