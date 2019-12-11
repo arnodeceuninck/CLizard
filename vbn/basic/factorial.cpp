b0458236e293e87b1739778bf051eb1c0dc9ed65
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2019-12-11 17:40:15 +0100

    Support for variable assignments (incl +=, ++, ...)

--- vbn/basic/factorial.cpp
+++ vbn/basic/factorial.cpp
@@ -1,14 +1,15 @@
 int factorial(int i) {
     if (i == 0) {
         return 1;
-    } else {
-        return i * factorial(i - 1);
+    } else { // TODO: else placed wrong in tree, probably terminal missing in list (PS: it's the 1)
+        i = i * factorial(i - 1);
+        return i;
         // hack hack hack Dorien was hier
     }
 }
 
 
 int main() {
-    string s = "yeet";
-    int a = factorial(9);
+    int i = 9;
+    std::cout << "The factorial of " << i << " is: " << factorial(i) << std::endl;
 }
