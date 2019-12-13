c572ad9dfe7a5671efa25f35211f4462a0042e44
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2019-12-14 00:25:53 +0100

    Support for header files (#ifndef, #define, #endif)

--- src/mainILR.cpp
+++ src/mainILR.cpp
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
 
 //    example(); // Assure this one keeps working when extending the grammar
 
-    std::string filename = "vbn/basic/includes.cpp";
+    std::string filename = "vbn/basic/test.h";
     AST ast(filename);
 
     filename = "output/parsed.dot";
---
+++ vbn/basic/test.h
@@ -0,0 +1,10 @@
+#ifndef CLIZARD_TEST_H
+#define CLIZARD_TEST_H
+
+
+class test {
+    int a;
+};
+
+
+#endif //CLIZARD_TEST_H
